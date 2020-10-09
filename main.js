544b3b270ee68d65f80c3b54d1aa73271be57061
gagan7gb <72530700+gagan7gb@users.noreply.github.com>
2020-10-09 23:57:25 +0530

    new

--- main.js
+++ main.js
@@ -5,10 +5,24 @@ $(document).ready(function () {
   });
 
    $('.top-nav .nav-link').on('click', function () {
-  	$('menu-toggler').removeClass('open');
+  	$('.menu-toggler').removeClass('open');
   	$('.top-nav').removeClass('open');
   });
+   $('nav a[href*="#"]').on('click', function () { 
+    $('html, body').animate({
+       scrollTop: $($(this).attr('href')).offset().top - 100
+       }, 2000);
+   });
 
- 
+   $('.up').on('click', function () { 
+    $('html, body').animate({
+       scrollTop: 0
+       }, 2000);
+
+ });
+   AOS.init({
+   	 easing: 'ease',
+   	 duration: 1800
+   });
 
 });
